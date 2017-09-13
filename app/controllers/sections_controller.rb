59c235130cde77ace9e22a56ba868ec546179d9b
gnyamach <gnyamach@msudenver.edu>
2017-09-12 19:00:15 -0600

    After creating the uniqueness and adding validation in the sections table and fixing the show form in the view folder

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,6 +1,6 @@
 class SectionsController < ApplicationController
   before_action :set_section, only: [:show, :edit, :update, :destroy]
-
+  
   # GET /sections
   # GET /sections.json
   def index
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,10 @@
 class Section < ApplicationRecord
   belongs_to :course
   belongs_to :professor
+  validates_uniqueness_of :name, scope: :course_id
+
+  validates :name, presence: true
+  validates :room, presence: true
+  validates :semester, presence: true
+
 end
