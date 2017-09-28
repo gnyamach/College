5a97c3f02012c739f55cab4786492ba08c595597
gnyamach <gnyamach@msudenver.edu>
2017-09-27 19:58:23 -0600

    after adding the search for the professors without using Ajax and testing it

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -4,7 +4,12 @@ class ProfessorsController < ApplicationController
   # GET /professors
   # GET /professors.json
   def index
-    @professors = Professor.all
+  #  @professors = Professor.all
+    @professors = if params[:enteredsearch]
+      Professor.where('name Like ?', "%#{params[:enteredsearch]}%")
+    else
+      Professor.all
+    end
   end
 
   # GET /professors/1
@@ -69,6 +74,6 @@ class ProfessorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def professor_params
-      params.require(:professor).permit(:name)
+      params.require(:professor).permit(:name, :enteredsearch)
     end
 end
